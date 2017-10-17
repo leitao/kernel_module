aa134623ca93ef26f7dc6ead5e66c7b509b79ada
Breno Leitao <brenohl@br.ibm.com>
2017-10-17 11:11:36 -0400

    Out of bound read also

--- hello.c
+++ hello.c
@@ -2,18 +2,22 @@
 #include <linux/kernel.h>	/* Needed for KERN_INFO */
 #include <linux/slab.h>
 
+void *p;
+
 int init_module(void)
 {
-	void *p;
+	int x = 0;
+
 	printk(KERN_INFO "Allocating memory.\n");
 	p = kmalloc(1024, GFP_KERNEL);
-
-	if (p)
+	if (p){
+		x = ((int *) p)[1024];
 		return 0;
-	return 1;
+	}	
+	return x;
 }
 
 void cleanup_module(void)
 {
-	printk(KERN_INFO "Goodbye world 1.\n");
+	printk(KERN_INFO "Not freeing p = %p.\n", p);
 }
